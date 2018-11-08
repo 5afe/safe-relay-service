718cc5d4c82798d0c79dbcd9b19fd9f33cfb64a6
Uxio Fuentefria <uxio@gnosis.pm>
2018-11-08 14:05:37 +0100

    Auto deploy master copy if using ganache

--- safe_relay_service/relay/management/commands/deploy_safe_master_copy.py
+++ safe_relay_service/relay/management/commands/deploy_safe_master_copy.py
@@ -3,13 +3,39 @@ from django.core.management.base import BaseCommand
 from gnosis.safe.safe_service import SafeServiceProvider
 
 
-# TODO Allow it to accept private keys
 class Command(BaseCommand):
-    help = 'Deploys master copy using first unlocked account on the node. For using it with `ganache -d`'
+    help = 'Deploys master copy using first unlocked account on the node if `ganache -d` is found and contract ' \
+           'is not deployed. If not you need to set a private key or an unlocked account on the node'
+    GANACHE_FIRST_ACCOUNT = '0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1'
+
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument('--deployer-key', help='Private key for deployer')
+        parser.add_argument('--deployer-account', help='Public key unlocked in node for deployer')
 
     def handle(self, *args, **options):
         safe_service = SafeServiceProvider()
-        account = safe_service.w3.eth.accounts[0]
+        account = safe_service.w3.eth.accounts[0] if safe_service.w3.eth.accounts else None
+        deployer_key = options['deployer_key']
+        deployer_account = options['deployer_account']
+        master_copy_address = None
+        if deployer_key:
+            self.stdout.write(self.style.SUCCESS('Deploying master copy using deployer key'))
+            master_copy_address = safe_service.deploy_master_contract(deployer_key=deployer_key)
+        elif deployer_account:
+            self.stdout.write(self.style.SUCCESS('Deploying master copy using deployer account'))
+            master_copy_address = safe_service.deploy_master_contract(deployer_account=deployer_account)
+        elif account == self.GANACHE_FIRST_ACCOUNT:
+            self.stdout.write(self.style.SUCCESS('Ganache detected, deploying master copy if not deployed'))
+            code = safe_service.w3.eth.getCode(safe_service.master_copy_address)
+            if code == b'\x00':
+                master_copy_address = safe_service.deploy_master_contract(deployer_account=account)
+            else:
+                self.stdout.write(self.style.NOTICE('Master copy already deployed'))
+        else:
+            self.stdout.write(self.style.NOTICE('Nothing done'))
 
-        master_copy_address = safe_service.deploy_master_contract(deployer_account=account)
-        self.stdout.write(self.style.SUCCESS('Master copy deployed on %s' % master_copy_address))
+        if master_copy_address:
+            self.stdout.write(self.style.SUCCESS('Master copy deployed on %s' % master_copy_address))
+        else:
+            self.stdout.write(self.style.NOTICE('Master copy not deployed'))
