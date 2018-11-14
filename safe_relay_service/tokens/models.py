5afca20d8464624509a735d69a0c47948fa6437a
Uxio Fuentefria <uxio@gnosis.pm>
2018-11-14 10:28:56 +0100

    Fix token price api retrieval

--- safe_relay_service/tokens/models.py
+++ safe_relay_service/tokens/models.py
@@ -6,6 +6,10 @@ from django.db import models
 from django_eth.models import EthereumAddressField
 
 
+class CannotGetTokenPriceFromApi(Exception):
+    pass
+
+
 class Token(models.Model):
     address = EthereumAddressField(primary_key=True)
     name = models.CharField(max_length=30)
@@ -25,7 +29,11 @@ class Token(models.Model):
     def get_eth_value(self) -> float:
         if self.fixed_eth_conversion is None:
             pair = '{}ETH'.format(self.code)
-            price = float(requests.get('https://api.kraken.com/0/public/Ticker?pair=' + pair).json()['result']['c'][0])
+            api_json = requests.get('https://api.kraken.com/0/public/Ticker?pair=' + pair).json()
+            error = api_json.get('error')
+            if error:
+                raise CannotGetTokenPriceFromApi(str(api_json['error']))
+            price = float(api_json['result'][pair]['c'][0])
             return price
         else:
             return float(self.fixed_eth_conversion)
