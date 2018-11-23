712a89e32f167b90bab66d2b9e3fa6bf75cddb4f
Uxio Fuentefria <uxio@gnosis.pm>
2018-11-23 18:00:06 +0100

    Improve admin

--- safe_relay_service/relay/admin.py
+++ safe_relay_service/relay/admin.py
@@ -2,4 +2,20 @@ from django.contrib import admin
 
 from .models import SafeContract, SafeCreation, SafeFunding, SafeMultisigTx
 
-admin.site.register([SafeContract, SafeCreation, SafeFunding, SafeMultisigTx])
+
+admin.site.register(SafeFunding)
+
+
+@admin.register(SafeContract)
+class SafeContractAdmin(admin.ModelAdmin):
+    list_display = ('address', 'master_copy')
+
+
+@admin.register(SafeCreation)
+class SafeCreationAdmin(admin.ModelAdmin):
+    list_display = ('safe', 'deployer', 'threshold', 'payment', 'payment_ether', 'payment_token')
+
+
+@admin.register(SafeMultisigTx)
+class SafeMultisigTxAdmin(admin.ModelAdmin):
+    list_display = ('safe', 'to', 'value', 'nonce', 'data')
