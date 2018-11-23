ca699f182e0075d9edf4a161e7ff2a4dc5b8886f
Uxio Fuentefria <uxio@gnosis.pm>
2018-11-23 18:17:36 +0100

    Improve admin

--- safe_relay_service/relay/admin.py
+++ safe_relay_service/relay/admin.py
@@ -16,6 +16,14 @@ class SafeCreationAdmin(admin.ModelAdmin):
     list_display = ('safe', 'deployer', 'threshold', 'payment', 'payment_ether', 'payment_token')
 
 
+@admin.register(SafeFunding)
+class SafeFundingAdmin(admin.ModelAdmin):
+    list_display = ('safe', 'safe_status', 'deployer_funded_tx_hash', 'safe_deployed_tx_hash')
+
+    def safe_status(self, obj: SafeFunding):
+        return obj.__str__()
+
+
 @admin.register(SafeMultisigTx)
 class SafeMultisigTxAdmin(admin.ModelAdmin):
     list_display = ('safe', 'to', 'value', 'nonce', 'data')
