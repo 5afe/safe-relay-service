b70bb8c23bacecbf298f3501e615c8c343999be9
Rob Moore <rmoore@blockcrushr.com>
2019-01-10 16:26:44 -0400

    - attempting to fix migrations to build safecreation table correctly.

--- safe_relay_service/relay/migrations/0001_initial.py
+++ safe_relay_service/relay/migrations/0001_initial.py
@@ -21,8 +21,10 @@ class Migration(migrations.Migration):
             fields=[
                 ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, editable=False, verbose_name='created')),
                 ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, editable=False, verbose_name='modified')),
-                ('address', django_eth.models.EthereumAddressField(primary_key=True, serialize=False)),
+                ('address', django_eth.models.EthereumAddressField()),
                 ('master_copy', django_eth.models.EthereumAddressField()),
+                ('salt', models.AutoField(auto_created=True, primary_key=True, serialize=False)),
+                ('subscription_module_address', django_eth.models.EthereumAddressField())
             ],
             options={
                 'abstract': False,
