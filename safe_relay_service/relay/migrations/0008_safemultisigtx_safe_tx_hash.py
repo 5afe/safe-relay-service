ad231ce85c928a856f85297946cc315b3a519827
Uxio Fuentefria <uxio@gnosis.pm>
2019-01-15 16:41:15 +0100

    Fix problem with tx hash migration

--- safe_relay_service/relay/migrations/0008_safemultisigtx_safe_tx_hash.py
+++ safe_relay_service/relay/migrations/0008_safemultisigtx_safe_tx_hash.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='safemultisigtx',
             name='safe_tx_hash',
-            field=gnosis.eth.django.models.Sha3HashField(default='', unique=True),
+            field=gnosis.eth.django.models.Sha3HashField(default=None, unique=True),
             preserve_default=False,
         ),
     ]
