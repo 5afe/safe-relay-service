065ca0d2f51fa7ed07a642b006c04c272af9cf35
Rob Moore <rmoore@blockcrushr.com>
2019-01-28 13:25:10 -0400

    Reverted changes to docker-compose, run_web, and manage.py that were accidentally committed

--- manage.py
+++ manage.py
@@ -7,9 +7,6 @@ if __name__ == '__main__':
 
     try:
         from django.core.management import execute_from_command_line
-        import ptvsd
-        if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
-            ptvsd.enable_attach(address=('0.0.0.0', 5678), redirect_output=True)
     except ImportError:
         # The above import may fail for some other reason. Ensure that the
         # issue is really that Django is missing to avoid masking other
