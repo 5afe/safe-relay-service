e3b28ee4df27b245b69e2259ef966d5b5f9b4537
Uxio Fuentefria <uxio@gnosis.pm>
2019-02-07 13:18:27 +0100

    Test CSRF trusted origins

--- config/settings/production.py
+++ config/settings/production.py
@@ -42,6 +42,9 @@ SESSION_COOKIE_HTTPONLY = True
 CSRF_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-httponly
 CSRF_COOKIE_HTTPONLY = True
+CSRF_TRUSTED_ORIGINS = [
+    'safe-relay.dev.gnosisdev.com',
+]
 # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
 # TODO: set this to 60 seconds first and then to 518400 once you prove the former works
