6f5edd7ea3dc59bab882c83aa7e96b0d0581a82a
Uxio Fuentefria <uxio@gnosis.pm>
2019-02-07 11:25:35 +0100

    Disable CSRF

--- config/settings/base.py
+++ config/settings/base.py
@@ -84,6 +84,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
+    'safe_relay_service.relay.csrf.DisableCSRF',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
