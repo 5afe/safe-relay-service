c57c4e15f3ae4d005742cf4dba5b687bdfd9ece0
Jason Barnett <jbarnett@Jasons-MacBook-Pro.local>
2019-02-11 11:48:46 -0400

    make prod deployment work

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,6 @@ schema_view = get_schema_view(
         license=openapi.License(name='MIT License'),
     ),
     validators=['flex', 'ssv'],
-    url='http://localhost:8000',
     public=True,
     # permission_classes=(permissions.AllowAny,),
 )
