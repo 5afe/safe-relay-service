2fc16b21c1028d0011b88ecffa8c7a4f188db81a
Uxio Fuentefria <uxio@gnosis.pm>
2019-05-14 18:00:57 +0200

    Refactor automatic task creation

--- safe_relay_service/relay/management/commands/setup_safe_relay.py
+++ safe_relay_service/relay/management/commands/setup_safe_relay.py
@@ -1,56 +1,45 @@
+from typing import NamedTuple, Tuple
+
 from django.core.management.base import BaseCommand
 
 from django_celery_beat.models import IntervalSchedule, PeriodicTask
+
+
+class CeleryTaskConfiguration(NamedTuple):
+    name: str
+    description: str
+    interval: int
+    period: str
+
+    def create_task(self) -> Tuple[PeriodicTask, bool]:
+        interval, _ = IntervalSchedule.objects.get_or_create(every=self.interval, period=self.period)
+        return PeriodicTask.objects.get_or_create(task=self.name,
+                                                  defaults={
+                                                      'name': self.description,
+                                                      'interval': interval
+                                                  })
 
 
 class Command(BaseCommand):
     help = 'Setup Safe relay required tasks'
 
     def handle(self, *args, **options):
-        task_name = 'safe_relay_service.relay.tasks.deploy_safes_task'
-        if PeriodicTask.objects.filter(task=task_name).count():
-            self.stdout.write(self.style.SUCCESS('Task %s was already created' % task_name))
-        else:
-            interval, _ = IntervalSchedule.objects.get_or_create(every=20, period='seconds')
-            PeriodicTask.objects.create(
-                name='Deploy Safes Task',
-                task=task_name,
-                interval=interval
-            )
-            self.stdout.write(self.style.SUCCESS('Created Periodic Task %s' % task_name))
+        tasks = [CeleryTaskConfiguration('safe_relay_service.relay.tasks.deploy_safes_task',
+                                         'Deploy Safes', 20, IntervalSchedule.SECONDS),
+                 CeleryTaskConfiguration('safe_relay_service.relay.tasks.check_balance_of_accounts_task',
+                                         'Check Balance of realy accounts', 1, IntervalSchedule.HOURS),
+                 CeleryTaskConfiguration('safe_relay_service.relay.tasks.check_create2_deployed_safes_task',
+                                         'Check and deploy Create2 Safes', 1, IntervalSchedule.MINUTES),
+                 CeleryTaskConfiguration('safe_relay_service.relay.tasks.find_internal_txs_task',
+                                         'Process Internal Txs for Safes', 2, IntervalSchedule.MINUTES),
+                 CeleryTaskConfiguration('safe_relay_service.relay.tasks.find_erc_20_721_transfers_task',
+                                         'Process ERC20/721 transfers for Safes', 2, IntervalSchedule.MINUTES),
+                 ]
 
-        task_name = 'safe_relay_service.relay.tasks.check_balance_of_accounts_task'
-        if PeriodicTask.objects.filter(task=task_name).count():
-            self.stdout.write(self.style.SUCCESS('Task %s was already created' % task_name))
-        else:
-            interval, _ = IntervalSchedule.objects.get_or_create(every=1, period='hours')
-            PeriodicTask.objects.create(
-                name='Check balance of relay accounts Task',
-                task=task_name,
-                interval=interval
-            )
-            self.stdout.write(self.style.SUCCESS('Created Periodic Task %s' % task_name))
+        for task in tasks:
+            _, created = task.create_task()
+            if created:
+                self.stdout.write(self.style.SUCCESS('Created Periodic Task %s' % task.name))
+            else:
+                self.stdout.write(self.style.SUCCESS('Task %s was already created' % task.name))
 
-        task_name = 'safe_relay_service.relay.tasks.check_create2_deployed_safes_task'
-        if PeriodicTask.objects.filter(task=task_name).count():
-            self.stdout.write(self.style.SUCCESS('Task %s was already created' % task_name))
-        else:
-            interval, _ = IntervalSchedule.objects.get_or_create(every=1, period='minutes')
-            PeriodicTask.objects.create(
-                name='Check if create2 safes were deployed ',
-                task=task_name,
-                interval=interval
-            )
-            self.stdout.write(self.style.SUCCESS('Created Periodic Task %s' % task_name))
-
-        task_name = 'safe_relay_service.relay.tasks.find_internal_txs_task'
-        if PeriodicTask.objects.filter(task=task_name).count():
-            self.stdout.write(self.style.SUCCESS('Task %s was already created' % task_name))
-        else:
-            interval, _ = IntervalSchedule.objects.get_or_create(every=2, period='minutes')
-            PeriodicTask.objects.create(
-                name='Process Internal txs',
-                task=task_name,
-                interval=interval
-            )
-            self.stdout.write(self.style.SUCCESS('Created Periodic Task %s' % task_name))
