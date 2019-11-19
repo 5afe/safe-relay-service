501edf29f7d5081c7ec065f387e9aeaf676c66b8
Uxio Fuentefria <uxio@gnosis.pm>
2019-11-19 13:20:03 +0100

    Just autodeploy Safes created in last 10 days

--- config/settings/base.py
+++ config/settings/base.py
@@ -190,6 +190,8 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-result_serializer
 CELERY_RESULT_SERIALIZER = 'json'
+# http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-task_ignore_result
+CELERY_TASK_IGNORE_RESULT = True
 
 # Django REST Framework
 # ------------------------------------------------------------------------------
