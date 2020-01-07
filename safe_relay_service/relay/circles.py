9f6149ef79a2dfe19ccb89063070031c7af564c3
anon <sarah.anne.friend@gmail.com>
2020-01-07 07:51:08 -0500

    No hardcoded gas

--- safe_relay_service/relay/circles.py
+++ safe_relay_service/relay/circles.py
@@ -3,17 +3,36 @@ from logging import getLogger
 
 from gnosis.eth import EthereumClientProvider
 from ethereum.utils import (check_checksum)
+from safe_relay_service.relay.services import TransactionServiceProvider
 
 logger = getLogger(__name__)
 
 
 class Circles:
+
+    value = 0
+    data = "0x519c6377000000000000000000000000000000000000000000000"
+      + "0000000000000000020000000000000000000000000000000000000000"
+      + "0000000000000000000000007436972636c65730000000000000000000"
+      + "0000000000000000000000000000000"
+    operation = 0
+    gas_token = NULL_ADDRESS
+
     magic_signup_gas = 3999092000000000
-    gas_price = 1
+
+    gas_price = 1 #gas price when paid in circles token
     ethereum_client = EthereumClientProvider()
 
-    def estimate_signup_gas(self):
-        return self.magic_signup_gas
+    def estimate_signup_gas(self, address):
+        '''estimates gas costs of circles token deployment using standard signup data string'''
+        transaction_estimation = TransactionServiceProvider().estimate_tx(
+            address,
+            settings.CIRCLES_HUB_ADDRESS,
+            value,
+            data,
+            operation,
+            gas_token)
+        return transaction_estimation
 
     def estimate_gas_price(self):
         return self.gas_price
