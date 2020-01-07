c88e1b35476fe921587755f7a0717008184ff286
anon <sarah.anne.friend@gmail.com>
2020-01-07 08:53:58 -0500

    Separate circles code

---
+++ safe_relay_service/relay/services/circles_service.py
@@ -0,0 +1,23 @@
+from .transaction_service import TransactionServiceProvider
+
+class CirclesService:
+    
+    def __init__(self):
+        self.value = 0
+        self.data = """0x519c6377000000000000000000000000000000000000000000000
+            0000000000000000020000000000000000000000000000000000000000
+            0000000000000000000000007436972636c65730000000000000000000
+            0000000000000000000000000000000"""
+        self.operation = 0
+        self.gas_token = NULL_ADDRESS
+
+    def estimate_signup_gas(self, address):
+        '''estimates gas costs of circles token deployment using standard signup data string'''
+        transaction_estimation = TransactionServiceProvider().estimate_tx(
+            address,
+            settings.CIRCLES_HUB_ADDRESS,
+            self.value,
+            self.data,
+            self.operation,
+            self.gas_token)
+        return transaction_estimation
