227f5478b7abd0a367f0b0d5daf1af7959cf5586
anon <sarah.anne.friend@gmail.com>
2020-01-07 12:24:43 -0500

    Now there are two funding tasks

--- safe_relay_service/relay/services/circles_service.py
+++ safe_relay_service/relay/services/circles_service.py
@@ -1,18 +1,26 @@
+from django.conf import settings
+from logging import getLogger
+
+from gnosis.eth.constants import NULL_ADDRESS
 from .transaction_service import TransactionServiceProvider
+
+logger = getLogger(__name__)
 
 class CirclesService:
 
     def __init__(self):
         self.value = 0
-        self.data = """0x519c6377000000000000000000000000000000000000000000000
-            0000000000000000020000000000000000000000000000000000000000
-            0000000000000000000000007436972636c65730000000000000000000
-            0000000000000000000000000000000"""
+        self.data = ("0x519c6377000000000000000000000000000000000000000000000"
+            "0000000000000000020000000000000000000000000000000000000000"
+            "0000000000000000000000007436972636c65730000000000000000000"
+            "0000000000000000000000000000000")
         self.operation = 0
         self.gas_token = NULL_ADDRESS
 
     def estimate_signup_gas(self, address):
         '''estimates gas costs of circles token deployment using standard signup data string'''
+        logger.error("In estimate signup gas")
+        logger.error('address %s data %s', address, self.data)
         transaction_estimation = TransactionServiceProvider().estimate_tx(
             address,
             settings.CIRCLES_HUB_ADDRESS,
@@ -20,4 +28,5 @@ class CirclesService:
             self.data,
             self.operation,
             self.gas_token)
+        logger.error('safe_tx_gas %d base_gas %d gas_price %d', transaction_estimation.safe_tx_gas, transaction_estimation.base_gas, transaction_estimation.gas_price)
         return (transaction_estimation.safe_tx_gas + transaction_estimation.base_gas) * transaction_estimation.gas_price
