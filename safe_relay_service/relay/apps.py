8d4eb604053b376bfbf2bff3c5a359985d4a25d6
David Albela Pérez <3659067+davidalbela@users.noreply.github.com>
2020-01-21 18:28:47 +0100

    Add Slack notification on startup

--- safe_relay_service/relay/apps.py
+++ safe_relay_service/relay/apps.py
@@ -1,5 +1,28 @@
 from django.apps import AppConfig
+from django.conf import settings
+from logging import getLogger
+
+from .utils import EthereumNetwork
+from ..version import __version__
+
+logger = getLogger(__name__)
+ethereum_network = EthereumNetwork()
 
 
 class RelayConfig(AppConfig):
     name = 'safe_relay_service.relay'
+
+    def ready(self):
+        from .services import SlackNotificationClientProvider
+
+        slack_notification_client = SlackNotificationClientProvider()
+        startup_message = str.format("Starting {} version {} on {}",
+                        self.verbose_name, __version__,
+                        ethereum_network.get_network())
+        logger.info(startup_message)
+        if settings.SLACK_API_WEBHOOK:
+            try:
+                slack_notification_client.send(startup_message)
+            except Exception:
+                logger.error("Cannot send slack notification for API webhook=%s",
+                             settings.SLACK_API_WEBHOOK, exc_info=True)
