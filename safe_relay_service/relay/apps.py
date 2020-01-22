6a08fa35b28a8af7e9de6a6b6f2a03321de97e04
David Albela Pérez <3659067+davidalbela@users.noreply.github.com>
2020-01-22 18:18:56 +0100

    Add Send Slack notification command task
      - Remove send Slack nodtification in RelayConfig class from startup in
    ready method.
      - Add  Send Slack notification Command class.

--- safe_relay_service/relay/apps.py
+++ safe_relay_service/relay/apps.py
@@ -1,29 +1,6 @@
 from django.apps import AppConfig
-from django.conf import settings
-from logging import getLogger
-
-from gnosis.eth import EthereumClientProvider
-
-from ..version import __version__
-
-logger = getLogger(__name__)
 
 
 class RelayConfig(AppConfig):
     name = 'safe_relay_service.relay'
-
-    def ready(self):
-        from .services import SlackNotificationClientProvider
-        ethereum_client = EthereumClientProvider()
-
-        slack_notification_client = SlackNotificationClientProvider()
-        startup_message = str.format("Starting {} version {} on {}",
-                        self.verbose_name, __version__,
-                        ethereum_client.get_network_name().name.capitalize())
-        logger.info(startup_message)
-        if settings.SLACK_API_WEBHOOK:
-            try:
-                slack_notification_client.send(startup_message)
-            except Exception:
-                logger.error("Cannot send slack notification for API webhook=%s",
-                             settings.SLACK_API_WEBHOOK, exc_info=True)
+    verbose_name = 'Safe Relay'
---
+++ safe_relay_service/relay/management/commands/send_slack_notification.py
@@ -0,0 +1,29 @@
+from django.apps import apps
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from gnosis.eth.ethereum_client import EthereumNetworkName
+from gnosis.eth import EthereumClientProvider
+
+from ...services import SlackNotificationClientProvider
+from ....version import __version__
+
+
+class Command(BaseCommand):
+    help = 'Send slack notification'
+    ethereum_client = EthereumClientProvider()
+    slack_notification_client = SlackNotificationClientProvider()
+
+    def handle(self, *args, **options):
+        app_name = apps.get_app_config('relay').verbose_name
+        startup_message = str.format("Starting {} version {} on {}",
+                                     app_name, __version__,
+                                     EthereumNetworkName(int(self.ethereum_client.w3.net.version)).name.capitalize())
+
+        self.stdout.write(self.style.SUCCESS(startup_message))
+        if settings.SLACK_API_WEBHOOK:
+            try:
+                self.slack_notification_client.send(startup_message)
+            except Exception:
+                self.stdout.write(self.style.ERROR("Cannot send slack notification for API webhook=%s",
+                                                   settings.SLACK_API_WEBHOOK, exc_info=True))
