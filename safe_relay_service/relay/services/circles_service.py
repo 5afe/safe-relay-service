caeccbfce00f111927772f2a7384697952d924ee
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-01-27 13:35:10 +0100

    Fix class method arguments

--- safe_relay_service/relay/services/circles_service.py
+++ safe_relay_service/relay/services/circles_service.py
@@ -12,18 +12,22 @@ class CirclesService:
 
     def __init__(self):
         self.value = 0
+
+        # tx data from Circles Token contract signup method
         self.data = ("0x519c6377000000000000000000000000000000000000000000000"
             "0000000000000000020000000000000000000000000000000000000000"
             "0000000000000000000000007436972636c65730000000000000000000"
             "0000000000000000000000000000000")
+
         self.operation = 0
         self.gas_token = NULL_ADDRESS
 
-    def estimate_signup_gas(self, address):
-        '''estimates gas costs of circles token deployment using standard signup data string'''
-        logger.info('address %s data %s', address, self.data)
+    def estimate_signup_gas(self, safe_address: str):
+        """
+        Estimates gas costs of circles token deployment using standard signup data string
+        """
         transaction_estimation = TransactionServiceProvider().estimate_tx(
-            address,
+            safe_address,
             settings.CIRCLES_HUB_ADDRESS,
             self.value,
             self.data,
