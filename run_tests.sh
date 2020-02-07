0535db11b051eabe93424f32a3cd0cfc7d6d0f96
Uxio Fuentefria <uxio@gnosis.pm>
2020-02-07 10:56:49 +0100

    Update run_tests file

--- run_tests.sh
+++ run_tests.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-docker-compose build --force-rm
-docker-compose up --no-start db redis ganache
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml build --force-rm db redis ganache
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --no-start db redis ganache
 docker restart safe-relay-service_db_1 safe-relay-service_redis_1 safe-relay-service_ganache_1
 sleep 2
-DJANGO_DOT_ENV_FILE=.env_local pytest
+DJANGO_SETTINGS_MODULE=config.settings.test DJANGO_DOT_ENV_FILE=.env_local pytest
--- run_tests_rinkeby.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-DJANGO_SETTINGS_MODULE=config.settings.test_rinkeby DJANGO_DOT_ENV_FILE=.env_rinkeby pytest safe_relay_service/relay/tests/test_views.py
