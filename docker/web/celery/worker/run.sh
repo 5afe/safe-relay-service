300ff4d6c3f8917e4204268a79d1f09888df3738
Uxio Fuentefria <uxio@gnosis.pm>
2020-04-02 12:18:43 +0200

    Update nginx and run scripts

--- docker/web/celery/worker/run.sh
+++ docker/web/celery/worker/run.sh
@@ -2,13 +2,15 @@
 
 set -euo pipefail
 
-# DEBUG set in .env
+# DEBUG set in .env_docker_compose
 if [ ${DEBUG:-0} = 1 ]; then
     log_level="debug"
 else
     log_level="info"
 fi
 
-sleep 5
-echo "==> Running Celery worker <=="
+echo "==> $(date +%H:%M:%S) ==> Migrating Django models... "
+python manage.py migrate --noinput
+
+echo "==> $(date +%H:%M:%S) ==> Running Celery worker <=="
 exec celery -A safe_relay_service.taskapp worker --loglevel $log_level -c 4
