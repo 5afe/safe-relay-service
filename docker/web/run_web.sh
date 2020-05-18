0423bf20c7ef4462c76350e832337c8ab33d7e2c
Uxio Fuentefria <uxio@gnosis.pm>
2020-05-18 16:40:03 +0200

    Add again the auto deploy of Safe contracts

--- docker/web/run_web.sh
+++ docker/web/run_web.sh
@@ -16,5 +16,10 @@ STATIC_ROOT=$DOCKER_SHARED_DIR/staticfiles python manage.py collectstatic --noin
 echo "==> $(date +%H:%M:%S) ==> Send via Slack info about service version and network"
 python manage.py send_slack_notification
 
+if [ "${DEPLOY_MASTER_COPY_ON_INIT:-0}" = 1 ]; then
+  echo "==> $(date +%H:%M:%S) ==> Deploy Safe master copy..."
+  python manage.py deploy_safe_contracts
+fi
+
 echo "==> $(date +%H:%M:%S) ==> Running Gunicorn... "
 exec gunicorn --worker-class gevent --pythonpath "$PWD" config.wsgi:application --timeout 60 --graceful-timeout 60 --log-file=- --error-logfile=- --access-logfile=- --log-level info --logger-class='safe_relay_service.relay.utils.CustomGunicornLogger' -b unix:$DOCKER_SHARED_DIR/gunicorn.socket -b 0.0.0.0:8888
