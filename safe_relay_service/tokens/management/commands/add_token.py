e05975c68d3a61449fc668612487f3d504701646
Uxio Fuentefria <uxio@gnosis.pm>
2020-07-29 16:40:46 +0200

    Improve add_token command

--- safe_relay_service/tokens/management/commands/add_token.py
+++ safe_relay_service/tokens/management/commands/add_token.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 
 from gnosis.eth import EthereumClientProvider
+from gnosis.eth.ethereum_client import InvalidERC20Info
 
 from ...models import Token
 
@@ -23,16 +24,21 @@ class Command(BaseCommand):
             token_address = ethereum_client.w3.toChecksumAddress(token_address)
             try:
                 token = Token.objects.get(address=token_address)
-                self.stdout.write(self.style.SUCCESS(f'Token {token.name} - {token.symbol} with address '
+                self.stdout.write(self.style.WARNING(f'Token {token.name} - {token.symbol} with address '
                                                      f'{token_address} already exists'))
                 continue
             except Token.DoesNotExist:
                 pass
-            info = ethereum_client.erc20.get_info(token_address)
-            if no_prompt:
-                response = 'y'
-            else:
-                response = input(f'Do you want to create a token {info} (y/n) ').strip().lower()
-            if response == 'y':
-                Token.objects.create(address=token_address, name=info.name, symbol=info.symbol, decimals=info.decimals)
-                self.stdout.write(self.style.SUCCESS(f'Created token {info.name} on address {token_address}'))
+            try:
+                info = ethereum_client.erc20.get_info(token_address)
+                if no_prompt:
+                    response = 'y'
+                else:
+                    response = input(f'Do you want to create a token {info} (y/n) ').strip().lower()
+                if response == 'y':
+                    Token.objects.create(address=token_address, name=info.name, symbol=info.symbol,
+                                         decimals=info.decimals)
+                    self.stdout.write(self.style.SUCCESS(f'Created token {info.name} on address {token_address}'))
+            except InvalidERC20Info:
+                self.stdout.write(self.style.ERROR(f'Token with address {token_address} is not valid'))
+
