bae16bcc9ed920d53a120577b016312bb8d0a548
anon <sarah.anne.friend@gmail.com>
2020-10-17 07:37:50 -0400

    Raise the amount a little

--- safe_relay_service/relay/services/transaction_service.py
+++ safe_relay_service/relay/services/transaction_service.py
@@ -323,7 +323,7 @@ class TransactionService:
         """
         # Tx data from Circles Hub contract `signup` method
         data = ("0xb7bc0f73")
-        return 1342816999990500
+        return 1342817000000000
 
     def estimate_circles_organization_signup_tx(self, safe_address: str,
                                                 gas_token: str = NULL_ADDRESS) -> int:
