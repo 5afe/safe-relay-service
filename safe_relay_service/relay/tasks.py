e92b770818476750b79c6a9aea15ff8b943efd09
anon <sarah.anne.friend@gmail.com>
2020-10-28 08:53:43 -0400

    Add retry to funding service

--- safe_relay_service/relay/tasks.py
+++ safe_relay_service/relay/tasks.py
@@ -499,7 +499,8 @@ def circles_onboarding_safe_task(self, safe_address: str) -> None:
 
                     FundingServiceProvider().send_eth_to(safe_address,
                                                          payment,
-                                                         gas=24000)
+                                                         gas=24000,
+                                                         retry=true)
                     # Retry later to check for enough funding and successful deployment
                     raise self.retry(countdown=30)
                 else:
